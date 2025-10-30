1e270f887a43a049ce081918c35fcb1a35b61494
Web2h <web2h@yahoo.com>
2025-10-30 17:34:44 -0400

    Improved welcome part

--- src/App.js
+++ src/App.js
@@ -8,33 +8,38 @@ function App() {
   return (
     <div className="App">
   <header className="header">
-        <button
-          className={`hamburger${navOpen ? ' open' : ''}`}
-          aria-label="Toggle navigation"
-          onClick={() => setNavOpen(!navOpen)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-        <nav className={`nav nav-top${navOpen ? ' nav-open' : ''}`}>
-          <img src={require('./images/logo.png')} alt="Logo" className="welcome-logo" />
-          <a href="#about" onClick={() => setNavOpen(false)}>About</a>
-          <a href="#services" onClick={() => setNavOpen(false)}>Services</a>
-          <a href="#contact" onClick={() => setNavOpen(false)}>Contact</a>
-          
-        </nav>
+
+        <div className="navigation">
+          <img src={require('./images/logo.png')} alt="Logo" className="header-logo" />
+          <button 
+            className={`hamburger${navOpen ? ' open' : ''}`}
+            onClick={() => setNavOpen(!navOpen)}
+            aria-label="Toggle navigation"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <nav className={`nav nav-top${navOpen ? ' nav-open' : ''}`}>
+            <a href="#about" onClick={() => setNavOpen(false)}>A propos</a>
+            <a href="#services" onClick={() => setNavOpen(false)}>Mes services</a>
+            <a href="#contact" onClick={() => setNavOpen(false)}>Contact</a>
+          </nav>
+        </div>
         
         <div className="welcome-block">
           <div className="welcome-content">
             <div className="welcome-flex">
-              <h1>Arlette RH Solutions</h1>
+              <h1>Arlette <span>RH</span> Solutions</h1>
             </div>
           </div>
-          <p className="welcome-citation">Solutions RH / Marketing complètes sous un même toit</p>
+          <p className="welcome-citation">Consultante marketing RH et intégration professionnelle</p>
+
+
+          <p className="welcome-citation-2">Révolutionnons ensemble votre attractivité et votre marque employeur!</p>
         </div>
         
-        <button className="soumission-btn soumission-btn-fixed">Demander une soumission</button>
+        <button className="soumission-btn soumission-btn-fixed">Consultation gratuite 30 min</button>
         
       </header>
       <main className="main">
